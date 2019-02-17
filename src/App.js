32941abac5bfc6299328bd766bbb36519d44adbc
Marty Powell <jpowel25@gmail.com>
2019-02-17 11:21:01 -0500

    added input filter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import servicesData from "./data/services.json";
 import { List, Card, Icon, Input } from "antd";
 
 const App = () => {
-  const [services, initState] = useState(servicesData);
+  const [services, setServices] = useState(servicesData);
+  const [filteredServices, setFilteredServices] = useState(services);
+
+  const handleInputFilterChange = changeEvent => {
+    const { value } = changeEvent.currentTarget;
+    const filterVal = value ? value.toLowerCase() : "";
+    const filteredServices = services.filter(service => {
+      return service.description.toLowerCase().indexOf(filterVal) > -1;
+    });
+
+    setFilteredServices(filteredServices);
+  };
+
   return (
     <div className="services">
       <h1>Request Service or Report a Problem</h1>
@@ -12,12 +24,13 @@ const App = () => {
         <Input
           size="large"
           placeholder="Filter services by name or category..."
+          onChange={handleInputFilterChange}
         />
       </section>
       <section class="section">
         <List
           grid={{ gutter: 16, column: 4 }}
-          dataSource={services}
+          dataSource={filteredServices}
           renderItem={service => (
             <List.Item>
               <Card
