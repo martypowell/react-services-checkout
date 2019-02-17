cb3684399ce24c339367e1b80afeccd25bea6fe5
Marty Powell <jpowel25@gmail.com>
2019-02-17 14:05:54 -0500

    added responsiveness to grid

--- src/App.js
+++ src/App.js
@@ -37,7 +37,15 @@ const App = () => {
       </section>
       <section className="section">
         <List
-          grid={{ gutter: 16, column: 4 }}
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 4,
+            lg: 4,
+            xl: 6,
+            xxl: 3
+          }}
           dataSource={filteredServices}
           renderItem={service => (
             <List.Item>
