97377635b40566cb93937215ef273c4ff959a24e
Marty Powell <jpowel25@gmail.com>
2019-02-17 11:08:38 -0500

    added list of services

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import servicesData from "./data/services.json";
+import { List, Card, Icon } from "antd";
 
-const App = () => (
-  <div className="services">
-        <h1>Request Service or Report a Problem</h1>
-        </div>
- );
+const App = () => {
+  const [services, initState] = useState(servicesData);
+  return (
+    <div className="services">
+      <h1>Request Service or Report a Problem</h1>
+      <List
+        grid={{ gutter: 16, column: 4 }}
+        dataSource={services}
+        renderItem={service => (
+          <List.Item>
+            <Card
+              title={service.description}
+              extra={service.isMostPopularService && <Icon type="star" />}
+            >
+              Card content
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
 
 export default App;
