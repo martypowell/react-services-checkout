49a6bd4b350851187e21b9e307cbbb58466dba69
Marty Powell <jpowel25@gmail.com>
2019-02-17 13:43:31 -0500

    rolled back refactor added category search

--- src/App.js
+++ src/App.js
@@ -6,51 +6,52 @@ import { List, Card, Icon, Input } from "antd";
 const App = () => {
   const [services, setServices] = useState(servicesData);
   const [filteredServices, setFilteredServices] = useState(services);
+  const [filterValue, setFilterValue] = useState("");
 
   const handleInputFilterChange = changeEvent => {
     const { value } = changeEvent.currentTarget;
+
     const filterVal = value ? value.toLowerCase() : "";
     const filteredServices = services.filter(service => {
-      return service.description.toLowerCase().indexOf(filterVal) > -1;
+      var hasDescriptionMatch =
+        service.description.toLowerCase().indexOf(filterVal) > -1;
+      var numberOfCategoryMatches = service.categories.filter(
+        category => category.toLowerCase().indexOf(filterVal) > -1
+      ).length;
+      return hasDescriptionMatch || !!numberOfCategoryMatches;
     });
 
+    setFilterValue(value);
     setFilteredServices(filteredServices);
   };
-
-  const Filter = () => (
-    <Input
-      size="large"
-      placeholder="Filter services by name or category..."
-      onChange={handleInputFilterChange}
-    />
-  );
-
-  const Services = () => (
-    <List
-      grid={{ gutter: 16, column: 4 }}
-      dataSource={filteredServices}
-      renderItem={service => (
-        <List.Item>
-          <Card
-            title={service.description}
-            extra={service.isMostPopularService && <Icon type="star" />}
-          >
-            <p>Content</p>
-            <span>{service.categories.join(", ")}</span>
-          </Card>
-        </List.Item>
-      )}
-    />
-  );
 
   return (
     <div className="services">
       <h1>Request Service or Report a Problem</h1>
-      <section class="section">
-        <Filter />
+      <section className="section">
+        <Input
+          size="large"
+          placeholder="Filter services by name or category..."
+          onChange={handleInputFilterChange}
+          value={filterValue}
+        />
       </section>
-      <section class="section">
-        <Services />
+      <section className="section">
+        <List
+          grid={{ gutter: 16, column: 4 }}
+          dataSource={filteredServices}
+          renderItem={service => (
+            <List.Item>
+              <Card
+                title={service.description}
+                extra={service.isMostPopularService && <Icon type="star" />}
+              >
+                <p>Content</p>
+                <span>{service.categories.join(", ")}</span>
+              </Card>
+            </List.Item>
+          )}
+        />
       </section>
     </div>
   );
