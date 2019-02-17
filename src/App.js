67924d27681cc01f14cccdd05d29ff3fdf38eeec
Marty Powell <jpowel25@gmail.com>
2019-02-17 11:11:49 -0500

    added categories and input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
 import servicesData from "./data/services.json";
-import { List, Card, Icon } from "antd";
+import { List, Card, Icon, Input } from "antd";
 
 const App = () => {
   const [services, initState] = useState(servicesData);
   return (
     <div className="services">
       <h1>Request Service or Report a Problem</h1>
+      <Input
+        size="large"
+        placeholder="Filter services by name or category..."
+      />
       <List
         grid={{ gutter: 16, column: 4 }}
         dataSource={services}
@@ -17,7 +21,8 @@ const App = () => {
               title={service.description}
               extra={service.isMostPopularService && <Icon type="star" />}
             >
-              Card content
+              <p>Content</p>
+              <span>{service.categories.join(", ")}</span>
             </Card>
           </List.Item>
         )}
